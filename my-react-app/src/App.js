a602b32a484f30c1e9b5148e5ca0ec5ef986c5f2
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-31 19:15:14 -0400

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,6 @@
 import './css/main.css';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import DonatePage from './components/DonatePage';
 import FundraiserPage from './components/FundraiserPage';
@@ -13,15 +13,15 @@ function App() {
   return (
     <Router>
       <div className="container">
-        <Switch>
+        <Routes>
           <Route exact path="/" component={HomePage} />
           <Route path="/donate" component={DonatePage} />
           <Route path="/fundraiser" component={FundraiserPage} />
-	  <Route path="/statistics" component={StatisticsPage} />
+          <Route path="/statistics" component={StatisticsPage} />
           <Route path="/jobs" component={JobsPage} />
           <Route path="/about" component={AboutPage} />
           <Route path="/faq" component={FAQPage} />
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
