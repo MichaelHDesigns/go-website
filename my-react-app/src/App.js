d161d355ccad6600ffb9a4843cb28f787c10fe83
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-31 19:51:26 -0400

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import DonatePage from './components/DonatePage';
 import FundraiserPage from './components/FundraiserPage';
-import JobsPage from './components/JobsPage';
 import AboutPage from './components/AboutPage';
 import FAQPage from './components/FAQPage';
 import StatisticsPage from './components/StatisticsPage';
@@ -18,7 +17,6 @@ function App() {
           <Route path="/donate" component={DonatePage} />
           <Route path="/fundraiser" component={FundraiserPage} />
           <Route path="/statistics" component={StatisticsPage} />
-          <Route path="/jobs" component={JobsPage} />
           <Route path="/about" component={AboutPage} />
           <Route path="/faq" component={FAQPage} />
         </Routes>
