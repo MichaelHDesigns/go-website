63fb0119aa0d234d97013cff6860a36b3c805e3c
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-31 20:40:19 -0400

    Update FAQPage.js

--- my-react-app/src/components/FAQPage.js
+++ my-react-app/src/components/FAQPage.js
@@ -7,7 +7,7 @@ function FaqPage() {
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/home">Home</Link>
         </li>
         <li>
           <div className="dropdown">
