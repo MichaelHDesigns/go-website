445899e2ff04a858e1a58f7880375296c95d148f
MichaelHDesigns <devilking6105@gmail.com>
2023-03-31 20:36:23 -0400

    fixing js files

--- my-react-app/src/components/DonatePage.js
+++ my-react-app/src/components/DonatePage.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import donationABI from '../abi/Donation.json';
+import DonationABI from '../abi/Donation.json';
 import { ethers } from 'ethers';
 import { JsonRpcProvider } from '@ethersproject/providers';
 
 const provider = new JsonRpcProvider('http://206.189.231.104:8545');
 const signer = provider.getSigner();
 
+// Replace the contract address below with your own deployed contract address
+const donationContractAddress = '0x13d624FE862C520f78A565DfD374710d85177eA2';
 
 const DonatePage = () => {
   const [donationAmount, setDonationAmount] = useState('');
@@ -15,7 +17,7 @@ const DonatePage = () => {
   const donate = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const donationContract = new ethers.Contract('0x13d624FE862C520f78A565DfD374710d85177eA2', donationABI, signer);
+    const donationContract = new ethers.Contract(donationContractAddress, DonationABI, signer);
 
     const donationValue = ethers.utils.parseEther(donationAmount);
     const donationTransaction = await donationContract.donate({ value: donationValue });
@@ -27,32 +29,32 @@ const DonatePage = () => {
 
   return (
     <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <div className="dropdown">
-            <button className="dropbtn">Give</button>
-            <div className="dropdown-content">
-              <Link to="/donate">Donate</Link>
-              <Link to="/fundraisers">Fundraisers</Link>
-              <Link to="/statistics">Statistics</Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <div className="dropdown">
+              <button className="dropbtn">Give</button>
+              <div className="dropdown-content">
+                <Link to="/donate">Donate</Link>
+                <Link to="/fundraisers">Fundraisers</Link>
+                <Link to="/statistics">Statistics</Link>
+              </div>
             </div>
-          </div>
-        </li>
-        <li>
-          <div className="dropdown">
-            <button className="dropbtn">Info</button>
-            <div className="dropdown-content">
-              <Link to="/about">About</Link>
-              <Link to="/faq">FAQ</Link>
+          </li>
+          <li>
+            <div className="dropdown">
+              <button className="dropbtn">Info</button>
+              <div className="dropdown-content">
+                <Link to="/about">About</Link>
+                <Link to="/faq">FAQ</Link>
+              </div>
             </div>
-          </div>
-        </li>
-      </ul>
-    </nav>
+          </li>
+        </ul>
+      </nav>
       <h1>Donate to our cause</h1>
       <p>Your donations will go directly to support our mission.</p>
       <form onSubmit={(e) => { e.preventDefault(); donate(); }}>
