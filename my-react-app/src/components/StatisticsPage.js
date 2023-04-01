3d313fc1d8e80c5122d9bd91bdcc93e3bd04f611
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-31 20:41:10 -0400

    Update StatisticsPage.js

--- my-react-app/src/components/StatisticsPage.js
+++ my-react-app/src/components/StatisticsPage.js
@@ -62,7 +62,7 @@ const StatisticsPage = () => {
      <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/home">Home</Link>
         </li>
         <li>
           <div className="dropdown">
